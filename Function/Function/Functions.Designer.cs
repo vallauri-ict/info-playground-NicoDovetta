32c7a5316494060f18329381e572afdd65aa2618
Dovetta Nicolas <nicodovetta@gmail.com>
2021-02-10 18:02:15 +0100

    Compito per il 10/02/2021

---
+++ Function/Function/Functions.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Function
+{
+	partial class Functions
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dgvDriver = new System.Windows.Forms.DataGridView();
+			this.dgvFunction1 = new System.Windows.Forms.DataGridView();
+			this.cmbNat = new System.Windows.Forms.ComboBox();
+			this.btnFunct1 = new System.Windows.Forms.Button();
+			this.btnFunction2 = new System.Windows.Forms.Button();
+			this.numPodium = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.dgvDriver)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dgvFunction1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numPodium)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dgvDriver
+			// 
+			this.dgvDriver.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvDriver.Location = new System.Drawing.Point(12, 12);
+			this.dgvDriver.Name = "dgvDriver";
+			this.dgvDriver.ScrollBars = System.Windows.Forms.ScrollBars.None;
+			this.dgvDriver.Size = new System.Drawing.Size(499, 190);
+			this.dgvDriver.TabIndex = 0;
+			// 
+			// dgvFunction1
+			// 
+			this.dgvFunction1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvFunction1.Location = new System.Drawing.Point(579, 12);
+			this.dgvFunction1.Name = "dgvFunction1";
+			this.dgvFunction1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+			this.dgvFunction1.Size = new System.Drawing.Size(499, 190);
+			this.dgvFunction1.TabIndex = 1;
+			// 
+			// cmbNat
+			// 
+			this.cmbNat.FormattingEnabled = true;
+			this.cmbNat.Location = new System.Drawing.Point(12, 234);
+			this.cmbNat.Name = "cmbNat";
+			this.cmbNat.Size = new System.Drawing.Size(48, 21);
+			this.cmbNat.TabIndex = 2;
+			// 
+			// btnFunct1
+			// 
+			this.btnFunct1.Location = new System.Drawing.Point(90, 232);
+			this.btnFunct1.Name = "btnFunct1";
+			this.btnFunct1.Size = new System.Drawing.Size(158, 23);
+			this.btnFunct1.TabIndex = 3;
+			this.btnFunct1.Text = "Load driver country";
+			this.btnFunct1.UseVisualStyleBackColor = true;
+			this.btnFunct1.Click += new System.EventHandler(this.btnFunct1_Click);
+			// 
+			// btnFunction2
+			// 
+			this.btnFunction2.Location = new System.Drawing.Point(90, 281);
+			this.btnFunction2.Name = "btnFunction2";
+			this.btnFunction2.Size = new System.Drawing.Size(158, 23);
+			this.btnFunction2.TabIndex = 4;
+			this.btnFunction2.Text = "Get number of pilot/s";
+			this.btnFunction2.UseVisualStyleBackColor = true;
+			this.btnFunction2.Click += new System.EventHandler(this.btnFunction2_Click);
+			// 
+			// numPodium
+			// 
+			this.numPodium.Location = new System.Drawing.Point(12, 284);
+			this.numPodium.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numPodium.Name = "numPodium";
+			this.numPodium.Size = new System.Drawing.Size(48, 20);
+			this.numPodium.TabIndex = 5;
+			this.numPodium.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// Functions
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1091, 386);
+			this.Controls.Add(this.numPodium);
+			this.Controls.Add(this.btnFunction2);
+			this.Controls.Add(this.btnFunct1);
+			this.Controls.Add(this.cmbNat);
+			this.Controls.Add(this.dgvFunction1);
+			this.Controls.Add(this.dgvDriver);
+			this.Name = "Functions";
+			this.Text = "Functions";
+			this.Load += new System.EventHandler(this.Functions_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dgvDriver)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dgvFunction1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numPodium)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dgvDriver;
+		private System.Windows.Forms.DataGridView dgvFunction1;
+		private System.Windows.Forms.ComboBox cmbNat;
+		private System.Windows.Forms.Button btnFunct1;
+		private System.Windows.Forms.Button btnFunction2;
+		private System.Windows.Forms.NumericUpDown numPodium;
+	}
+}
+
---
+++ Function/Function/Functions.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace Function
+{
+	public partial class Functions : Form
+	{
+		public static readonly string workingDirectory = Environment.CurrentDirectory;
+		public static readonly string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+		public static readonly string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + projectDirectory + @"\DriverDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+		private BindingSource bsDriver = new BindingSource();
+		private DataTable dtDriver;
+		private DataTable dtAus;
+		private SqlDataAdapter daDriver;
+		private SqlDataAdapter daAus;
+
+		public Functions()
+		{
+			InitializeComponent();
+		}
+
+		private void Functions_Load(object sender, EventArgs e)
+		{
+			using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+			{
+
+				using (SqlCommand command = new SqlCommand($"SELECT MAX(podiums_number) FROM Driver", connection))
+				{
+					connection.Open();
+
+					// create data adapter
+					numPodium.Maximum = Convert.ToInt32(command.ExecuteScalar());
+				}
+			}
+
+			dgvDriver.RowHeadersVisible = false;
+			dgvDriver.DataSource = bsDriver;
+			QueryDt("SELECT * FROM Driver", out daDriver);
+			if (daDriver != null)
+			{
+				dtDriver = new DataTable();
+				daDriver.Fill(dtDriver);
+				dgvDriver.DataSource = dtDriver;
+			}
+			dgvDriver.AutoResizeColumns();
+
+			List<string> lstNat = null;
+			Query_lstString("SELECT country FROM Driver", out lstNat);
+			if (lstNat != null)
+			{
+				cmbNat.DataSource = lstNat;
+			}
+		}
+
+		private void btnFunct1_Click(object sender, EventArgs e)
+		{
+			dgvFunction1.RowHeadersVisible = false;
+			dgvFunction1.DataSource = bsDriver;
+			QueryDt($"SELECT * FROM [dbo].getByNation('{cmbNat.SelectedItem}')", out daAus);
+			if (daAus != null)
+			{
+				dtAus = new DataTable();
+				daAus.Fill(dtAus);
+				dgvFunction1.DataSource = dtAus;
+			}
+			dgvFunction1.AutoResizeColumns();
+		}
+
+		private void btnFunction2_Click(object sender, EventArgs e)
+		{
+			using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+			{
+
+				using (SqlCommand command = new SqlCommand($"SELECT [dbo].getPodiumCount({numPodium.Value})", connection))
+				{
+					connection.Open();
+
+					int pilotNumber = (Int32)command.ExecuteScalar();
+					MessageBox.Show($"There {(pilotNumber > 1 ? "are" : "is")} {pilotNumber} pilot{(pilotNumber > 1 ? "s" : "")} that have equal or more than {numPodium.Value} podium{(numPodium.Value > 1 ? "s" : "")}","Function 2 - Result");
+				}
+			}
+		}
+
+		private void Query_lstString(string selectCommand, out List<string> lstNat)
+		{
+			lstNat = new List<string>();
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+				{
+
+					using (SqlCommand command = new SqlCommand(selectCommand, connection))
+					{
+						connection.Open();
+
+						// create data adapter
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								string countryCode = reader.GetString(0);
+								if (!lstNat.Contains(countryCode))
+								{
+									lstNat.Add(countryCode);
+								}
+							}
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void QueryDt(string selectCommand, out SqlDataAdapter da)
+		{
+			da = null;
+			try
+			{
+				// Crea un nuovo data adapter basato su selectCommand
+				da = new SqlDataAdapter(selectCommand, CONNECTION_STRING);
+
+				// Creo un command builder per generare un comando SQL update, insert, delete
+				SqlCommandBuilder commandBuilder = new SqlCommandBuilder(da);
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+	}
+}
